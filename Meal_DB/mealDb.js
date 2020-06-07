d9f2b849f5f0fae7aff3c26970dbf5556f899acf
Ajay Jinka <ajay.jinka@gmail.com>
2020-06-07 20:59:38 +0530

    masai-sprint-3

--- Meal_DB/mealDb.js
+++ Meal_DB/mealDb.js
@@ -3,7 +3,6 @@ function getData(displayFunction, firstLetter){
     
     var xhr = new XMLHttpRequest();
     var url = "https://www.themealdb.com/api/json/v1/1/search.php"
-    console.log(url + "?f=" + firstLetter)
     var result = null;
     xhr.open('GET', url + "?f=" + firstLetter)
     xhr.send();
@@ -23,31 +22,144 @@ function getData(displayFunction, firstLetter){
 var printData = function(input){
 
     input = JSON.parse(input)
-
-    console.log(input)
-
-    var body = document.querySelector('body')
     var div = document.getElementById('display')
-    div.innerHTML = ""
+    var select = document.getElementById('dropDown')
 
+    div.innerHTML = ""
     var h3 = document.createElement('h3')
     h3.innerText = "Choose the recipe of your choice"
-
     div.append(h3)
+
+    select.innerHTML = ""
+    var optnDefault = document.createElement('option')
+    optnDefault.innerText = "------------Select your heart's choice :) ------------"
+    select.append(optnDefault)
 
     for( var i = 0; i < input.meals.length; i++ ) {
 
-        if( i === 0 ) {
-            var select = document.createElement('select')
-        }
         var option = document.createElement('option')
         option.innerText = input.meals[i].strMeal
-        //console.log(option)
-        //console.log(input.meals[i].strMeal)
+        option.setAttribute('value',input.meals[i].strMeal)
         select.append(option)
     }
+    select.style.visibility = 'visible'
     div.append(select)
-    body.append(div)
+}
+
+function createCard(displayFunction, item){
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://www.themealdb.com/api/json/v1/1/search.php"
+    var result = null;
+    xhr.open('GET', url + "?s=" + item)
+    xhr.send();
+
+    xhr.onload = function() {
+        if( xhr.status === 200 ) {
+            result = xhr.response
+            displayFunction(result)
+        }
+        else {
+            console.log("Error Code is : " + xhr.status)
+        }
+    }
+
+}
+
+function printCard(input){
+    
+    input = JSON.parse(input)
+
+    var container  = document.getElementById('container')
+
+    container.innerHTML = ""
+
+    var cont0  = document.createElement('div')
+    cont0.setAttribute('id','cont0')
+
+    var cont1  = document.createElement('div')
+    cont1.setAttribute('id', 'cont1')
+
+    var divName = document.createElement('div')
+    var h3 = document.createElement('h3')   
+    h3.innerText = "Name of the Meal Recipe : " + input.meals[0].strMeal
+    cont0.append(h3)
+    var img = document.createElement('img')
+    img.src = input.meals[0].strMealThumb
+    img.style.width = "250px"
+    divName.append(img) 
+
+    cont1.append(divName)
+
+    var cont2  = document.createElement('div')
+    cont2.setAttribute('id','cont2')
+    var objIngred = {}
+    var objMeasure = {}
+    var i = 0
+    var j = 0
+    var objFinal = {}
+
+    for( key in input.meals[0] ) {
+        if( key.slice(0,13) === "strIngredient"  &&  input.meals[0][key] !== "" && input.meals[0][key] !== null ) {
+            objIngred[i] = input.meals[0][key]
+            i++
+        }
+    }
+
+    for( key in input.meals[0] ) {
+        if( key.slice(0,10) === "strMeasure" && input.meals[0][key] !== "" && input.meals[0][key] !== null ) {
+            objMeasure[j] = input.meals[0][key];
+            j++;
+        }
+    }
+
+    var len = Object.keys(objIngred).length;
+    
+    for( var k = 0; k < len; k++ ) {
+        objFinal[objIngred[k]] = objMeasure[k];
+    }
+    
+    var hIngred = document.createElement('h3')
+    hIngred.innerText = "The Ingredients : "
+    cont2.append(hIngred)
+
+    for( key in objFinal ) {
+
+        var para = document.createElement('p')
+        para.innerText = key + " : " + objFinal[key]
+        cont2.append(para)
+    }
+
+    cont0.append(cont1,cont2)
+
+    var cont3 = document.createElement('div')
+    cont3.setAttribute('id','cont3')
+
+    var hRecipe = document.createElement('h3')
+    hRecipe.innerText = "The Recipe : "
+    var paraRecipe = document.createElement('p')
+    paraRecipe.innerText = input.meals[0].strInstructions
+
+    cont3.append(hRecipe,paraRecipe)
+
+    var cont4 = document.createElement('div')
+    cont4.setAttribute('id','cont4')
+
+    var ytube = document.createElement('h3')
+    ytube.innerText = "Wanna see the Recipe in Youtube???"
+    var video = document.createElement('video')
+    video.controls = true
+    video.width = "700"
+    video.height = "350"
+    var source = document.createElement('source')
+    source.src = input.meals[0].strYoutube + ".mp4"
+    source.type = "video/mp4"
+    video.append(source)
+
+    cont4.append(ytube,video)
+    
+    container.append(cont0,cont3,cont4)
+
 }
 
 var btn = document.getElementById('submit')
@@ -55,3 +167,12 @@ btn.addEventListener('click', function(){
     var letter = document.getElementById('giveName').value
     getData(printData, letter)
 })
+
+
+var select = document.querySelector('#dropDown')
+
+select.addEventListener('change', function(){
+    var selectedMeal = select.value
+    createCard(printCard, selectedMeal);
+}) 
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # masai-sprint-3
+
 Meal DB​
+
+From this project we can get the meal recipes of our choice.
+
+Step 1: We give the first letter of the name of meal recipe,we get list of recipe names available with that letter
+
+Step 2: We select the any meal recipe of our choice
+
+Step 3: The recipe thumb nail, ingredients, and the procedure including a video will be displayed.
